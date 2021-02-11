4d1cc90fdf66b7cc175016dc88e0e4ab6d6121f2
orshefi <or@tevel-tech.com>
2021-02-11 18:49:01 +0200

    changed to replace all

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (projectType){
     core.info(`New version: ${newVersion}`)
     if(newVersion.includes("-")){
         core.info(`Found underscores in ${newVersion}`)
-        newVersion = newVersion.replace('_', '-')
+        newVersion = newVersion.replaceAll('_', '-')
         core.info(`Corrected version: ${newVersion}`)
     }
     const cleanVersion = semver.clean(newVersion, { loose: true })
